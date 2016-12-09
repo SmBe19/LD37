9ccf2a9baf74be5d796ff84fde73419e6cd23e0c
SmBe19 <git@schmidb.ch>
2016-12-09 19:58:37 +0100

    Initial Project Setup

---
+++ android/src/com/smeanox/games/ld37/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.smeanox.games.ld37;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.smeanox.games.ld37.LD37;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD37(), config);
+	}
+}
---
+++ desktop/src/com/smeanox/games/ld37/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smeanox.games.ld37.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smeanox.games.ld37.LD37;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD37(), config);
+	}
+}
